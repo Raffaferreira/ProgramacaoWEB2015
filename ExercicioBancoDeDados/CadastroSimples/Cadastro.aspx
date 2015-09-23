4c244a7da341898c289007c995aca96895961033
unknown <aluno@Dell100-HC1Q0Y1.band.net>
2015-09-22 21:03:21 -0300

    Subindo exercicio novo de banco de dados

---
+++ ExercicioBancoDeDados/CadastroSimples/Cadastro.aspx
@@ -0,0 +1,56 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Cadastro.aspx.cs" Inherits="CadastroSimples.Cadastro" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+	<title>Cadastro</title>
+
+	<style type="text/css">
+		body {
+			margin: 5px;
+			padding: 5px;
+		}
+
+		p {
+		}
+
+		.cadaster-content {
+			border: 1px solid #000;
+			background-color: #EFEFEF;
+			text-align: center;
+			height: 100%;
+		}
+
+		.wrapper-content {
+			width: 100%;
+			height: 100%;
+		}
+	</style>
+
+</head>
+<body>
+
+	<form id="form1" runat="server">
+		<div class="cadaster-content">
+			<div class="wrapper-content">
+				<p>Nome: </p>
+				<asp:TextBox runat="server" ID="TxtNome"></asp:TextBox>
+				<p>Endereço: </p>
+				<asp:TextBox runat="server" ID="TxtEndereco"></asp:TextBox>
+				<p>E-mail: </p>
+				<asp:TextBox runat="server" ID="TxtEmail"></asp:TextBox>
+				<p>Nascimento: </p>
+				<asp:TextBox runat="server" ID="TxtNascimento"></asp:TextBox>
+				<p>Peso: </p>
+				<asp:TextBox runat="server" ID="TxtPeso"></asp:TextBox>
+				<p>
+					<asp:Button runat="server" ID="Cadastrar" Text="Cadastrar" OnClick="Cadastrar_Click" />
+				</p>
+			</div>
+			<asp:Label runat="server" ID="labelMensagem"></asp:Label>
+		</div>
+	</form>
+
+</body>
+</html>
---
+++ ExercicioBancoDeDados/CadastroSimples/Cadastro.aspx.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace CadastroSimples
+{
+	public partial class Cadastro : System.Web.UI.Page
+	{
+		SqlConnection cnn = new SqlConnection("Server=tcp:eot9dccau4.database.windows.net,1433;Database=testedb;User ID=alunos@eot9dccau4;Password=web2015$;Encrypt=True");
+		SqlCommand command;
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			if (Page.IsPostBack == false)
+			{
+
+			}
+
+		}
+
+		protected void Cadastrar_Click(object sender, EventArgs e)
+		{
+			string nome = TxtNome.Text;
+			string endereco = TxtEndereco.Text;
+
+			string email = TxtEmail.Text.Trim();
+			int arroba, ponto, arroba2;
+			arroba = email.IndexOf('@');
+			arroba2 = email.LastIndexOf('@');
+			ponto = email.LastIndexOf('.');
+
+			//Converter nascimento
+			string nascimento = TxtNascimento.Text;
+			DateTime nascimentoData;
+
+			//Converter Peso
+			string peso = TxtPeso.Text;
+			double pesoDouble;
+
+			if (nome.Length == 0)
+			{
+				labelMensagem.Text = "Existe algum campo em branco, verifque novamente!";
+				return;
+			}
+			if (endereco.Length == 0)
+			{
+				labelMensagem.Text = "Existe algum campo em branco, verifque novamente!";
+				return;
+			}
+			if (arroba <= 0 || ponto <= (arroba + 1) || ponto == (email.Length - 1) || arroba2 != arroba)
+			{
+				labelMensagem.Text = "Existe algum campo em branco, verifque novamente!";
+				return;
+			}
+			if (DateTime.TryParse(nascimento, System.Globalization.CultureInfo.GetCultureInfo("pt-br"), System.Globalization.DateTimeStyles.None, out nascimentoData) == false)
+			{
+				labelMensagem.Text = "Existe algum campo em branco, verifque novamente!";
+				return;
+			}
+			if (double.TryParse(peso, out pesoDouble) == false)
+			{
+				labelMensagem.Text = "Existe algum campo em branco, verifque novamente!";
+				return;
+			}
+			labelMensagem.Text = "";
+
+			cnn.Open();
+
+			command = new SqlCommand("INSERT INTO tbPessoa (Nome, Endereco, Email, Nascimento, Peso) VALUES (@a, @b, @c, @d, @e)", cnn);
+			command.Parameters.Add(new SqlParameter("@a", SqlDbType.VarChar));
+			command.Parameters.Add(new SqlParameter("@b", SqlDbType.VarChar));
+			command.Parameters.Add(new SqlParameter("@c", SqlDbType.VarChar));
+			command.Parameters.Add(new SqlParameter("@d", SqlDbType.Date));
+			command.Parameters.Add(new SqlParameter("@e", SqlDbType.Float));
+
+			command.Parameters[0].Value = nome;
+			command.Parameters[1].Value = endereco;
+			command.Parameters[2].Value = email;
+			command.Parameters[3].Value = nascimentoData;
+			command.Parameters[4].Value = pesoDouble;
+			command.ExecuteNonQuery();
+			command.Dispose();
+
+			cnn.Close();
+			cnn.Dispose();
+
+			labelMensagem.Text = "CADASTRADO COM SUCESSO";
+		}
+	}
+}
