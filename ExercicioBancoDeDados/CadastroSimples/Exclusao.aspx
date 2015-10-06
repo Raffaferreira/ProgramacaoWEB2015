4adaa5ced3d6324cb162682aa80a1fe2f87299f3
unknown <aluno@Dell11-861P0Y1.band.net>
2015-10-06 20:58:20 -0300

    Novo web forms de exclusão no projeto

---
+++ ExercicioBancoDeDados/CadastroSimples/Exclusao.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Exclusao.aspx.cs" Inherits="CadastroSimples.Exclusao" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+
+	<title>Exclusão</title>
+
+	<style type="text/css">
+		body {
+			margin: 5px;
+			padding: 5px;
+		}
+
+		p {
+		}
+
+		.cadaster-content {
+			border: 1px solid #000;
+			background-color: #EFEFEF;
+			text-align: center;
+			height: 100%;
+		}
+
+		.wrapper-content {
+			width: 100%;
+			height: 100%;
+		}
+	</style>
+
+</head>
+<body>
+
+	<form id="form1" runat="server">
+		<div class="cadaster-content">
+			<h1 style="text-align: center;">EXCLUSÃO DE CADASTRO</h1>
+			<div class="wrapper-content">
+				<asp:Label runat="server" ID="lblMsg"></asp:Label>
+				<br />
+				<asp:Button runat="server" ID="btnSIM" Text="Sim" OnClick="btnSIM_Click" />
+				<asp:Button runat="server" ID="btnNAO" Text="Nao" OnClick="btnNAO_Click" />
+			</div>
+		</div>
+	</form>
+
+</body>
+</html>
---
+++ ExercicioBancoDeDados/CadastroSimples/Exclusao.aspx.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CadastroSimples
+{
+	public partial class Exclusao : System.Web.UI.Page
+	{
+		SqlConnection conn = new SqlConnection("Server=tcp:eot9dccau4.database.windows.net,1433;Database=testedb;User ID=alunos@eot9dccau4;Password=web2015$;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;");
+		SqlCommand command;
+		SqlDataReader reader;
+
+		protected void Page_Load(object sender, EventArgs e)
+		{	
+			string nome = string.Empty;
+			int id;
+			if (int.TryParse(Request.QueryString["id"], out id) == false)
+			{
+				lblMsg.Text = "Id inválido ou não existe !";
+				btnSIM.Visible = false;
+				btnNAO.Visible = false;
+				return;
+			}
+
+			conn.Open();
+			command = new SqlCommand();
+			command.CommandText = "SELECT Nome FROM tbPessoa WHERE Id = @a";
+			command.Connection = conn;
+			command.Parameters.AddWithValue("@a", id);
+			reader = command.ExecuteReader();
+
+			if (reader.Read() == true)
+			{
+				nome = reader.GetString(0);
+				lblMsg.Text = "Deseja mesmo excluir registro com o Nome: " + nome + " - e com o ID: " + id + " ?";
+				btnSIM.Visible = true;
+				btnNAO.Visible = true;
+			}
+			else
+			{
+				lblMsg.Text = "Não existe usuário com este ID, tente novamente !";
+				btnSIM.Visible = false;
+				btnNAO.Visible = false;
+			}
+
+			reader.Close();
+			reader.Dispose();
+			command.Dispose();
+
+			conn.Close();
+			conn.Dispose();
+		}
+
+		protected void btnSIM_Click(object sender, EventArgs e)
+		{
+			int id;
+			if (int.TryParse(Request.QueryString["id"], out id) == false)
+			{
+				//Nunca vai acontencer, proque já tem o id lá em cima, selecionado na mémoria
+				return;
+			}
+			
+			conn.Open();
+			command = new SqlCommand();
+			command.CommandText = "DELETE FROM tbPessoa WHERE Id = @a";
+			command.Connection = conn;
+			command.Parameters.AddWithValue("@a", id);
+			command.ExecuteNonQuery();
+			command.Dispose();
+
+			conn.Close();
+			conn.Dispose();
+
+			lblMsg.Text = "PESSOA EXCLUÍDA";
+		}
+
+		protected void btnNAO_Click(object sender, EventArgs e)
+		{
+			Response.Redirect("Cadastro.aspx");
+		}
+
+
+
+	}
+}
